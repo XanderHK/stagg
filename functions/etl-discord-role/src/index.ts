26ba84b26a4f0a8eddc4b8e4334e301919631d14
Dan Lindsey <dan@mdlindsey.com>
2021-03-04 10:15:20 -0500

    more logs on discord role etl

--- functions/etl-discord-role/src/index.ts
+++ functions/etl-discord-role/src/index.ts
@@ -4,9 +4,13 @@ import { runJob } from './worker'
 import { config } from './config'
 
 export default async (req, res) => {
+    console.log('[>] Starting Discord Role Rank ETL...')
     await useConfig(config)
+    console.log('[>] Configured Discord Role Rank ETL...')
     setNetworkConfig(config.network)
+    console.log('[>] Network config set for API library...')
     try { await validateNetworkAuth(req,res) } catch(e) { return }
+    console.log('[>] Network key validated, proceeding...')
     const { discord_id, limit, skip } = req.query as {[key:string]:string}
     console.log('[>] Running Discord rank role update:', discord_id, limit, skip)
     await runJob(discord_id, limit, skip)
